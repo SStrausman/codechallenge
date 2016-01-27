57c0eebf52b72c7314c9a5fa63b0b263b6967969
shea <sstrausman@gmail.com>
2016-01-27 13:13:16 -0600

    validations and relationships for assessment added

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,2 +1,8 @@
 class Assessment < ActiveRecord::Base
+	has_many :questions
+	has_many :user_assessments
+	has_many :users, through: :user_assessments
+
+	validates :title, presence: true
+	validates :title, uniqueness: true
 end
