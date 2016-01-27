7e131e9a761f0e8883db12572495c97583a04d05
shea <sstrausman@gmail.com>
2016-01-27 14:25:49 -0600

    retailer validations and relationships added

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,2 +1,11 @@
 class Retailer < ActiveRecord::Base
+	has_one :user
+	has_many :events
+	has_many :sales, through: :events
+
+	validates :point_of_contact, presence: true
+	validates :contact_email, presence: true
+	validates :zip_code, presence: true
+	validates :retailer_name, presence: true
+	validates :zip_code, format: ( with: /^\d{5}(?:[-\s]\d{4})?$)/ )
 end
