1f3ca8779adadd1ebd6a6a49cfef0d3f987ece01
shea <sstrausman@gmail.com>
2016-01-27 13:39:34 -0600

    brands and beverages added

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,2 +1,5 @@
 class Beverage < ActiveRecord::Base
+	belongs_to :brand
+	has_many :event_beverages
+	has_many :events, through: :event_beverages
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,12 @@
 class Brand < ActiveRecord::Base
+	
+	has_one :user
+	has_many :beverages
+	has_many :events, through: :users
+
+	validates :point_of_contact, presence: true 
+	validates :contact_email, presence: true
+	validates :brand_name, presence: true
+	validates :brand_name, uniqueness: true
+
 end
